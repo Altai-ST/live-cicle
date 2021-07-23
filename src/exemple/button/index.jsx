033849bec03fd356c91f875dbaa54b276492ace9
Altai <altaistamov2003@gmail.com>
2021-07-23 17:20:02 +0600

    first commit

---
+++ src/exemple/button/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import './button.css'
+
+export class ClickButton extends React.Component {
+              
+    constructor(props) {
+        super(props);
+        this.state = {class: "off", label: "Нажми"};
+           
+        this.press = this.press.bind(this);
+           
+        console.log("constructor");
+    }
+    static getDerivedStateFromProps(props, state) {
+         console.log("getDerivedStateFromProps()");
+         return null;
+    }
+    componentDidMount(){
+        console.log("componentDidMount()");
+    }
+    componentWillUnmount(){
+        console.log("componentWillUnmount()");
+    }
+    shouldComponentUpdate(){
+        console.log("shouldComponentUpdate()");
+        return true;
+    }
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+       console.log("getSnapshotBeforeUpdate()");
+       return null;
+   }
+    componentDidUpdate(){
+        console.log("componentDidUpdate()");
+    }
+    press(){
+        var className = (this.state.class==="off")?"on"
+        :"off";
+        this.setState({class: className});
+        let cool = (this.state.class==="off")?'Уже нажата'
+        :'Нажми';
+        this.setState({label: cool});
+    }
+    render() {
+        console.log("render()");
+        return <button onClick={this.press} className={this.state.class}>{this.state.label}</button>;
+    }
+}
